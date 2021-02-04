9444dba72cf0bc12c1489a2ed2c2990d388616bc
Aleksndr <izykov17@gmail.com>
2021-02-04 19:21:48 +0300

    first commit

---
+++ champ.js
@@ -0,0 +1,54 @@
+"use strict";
+            // 007Общение с пользователем
+
+// // alert('Hello');
+// const result=confirm("Are you here$?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?","");
+// console.log(answer +5);
+
+// const answers=[];
+//  answers[0]=prompt('Как ваше имя?','');
+//  answers[1]=prompt('Как ваша фамилия?','');
+//  answers[2]=prompt('Сколько вам лет','');
+
+// console.log(typeof(answers));
+
+            // 008Интерполяция 
+
+// const categr='toys';
+// console.log(`https//:someurl.com/${categr}/5`);
+
+// const user="Sasha";
+// alert( `Привет,${user}`);
+
+
+        // 009 Операторы в js
+
+// // console.log('arr'+ "-object");
+// // console.log(4+ "5");
+
+// let increment=10,
+//     dcr=10;
+
+//     // ++increment;
+//     // --dcr;
+//     console.log(++increment);
+//     console.log(--dcr);
+
+// console.log(5%2);
+//                 // // =  присваивание 
+//                 // // == сравнение равно или нет
+//                 // // === строгое сравнение(учитывается также и тип данных )
+// console.log(2+2*2!=='6');
+
+//         // && оператор 'И'; работает тогда когда оба правдивы
+//         // || оператор 'ИЛИ'; работает тогда когда один из них правда
+//         // !оператор отрицаания
+//         const isCheked=true,
+//         isClose=false;
+//         console.log (isCheked||!isClose);
+
+
+
